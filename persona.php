c308686cdcc3971688f6bfa0b221d0a2c3592aed
William Castro <wcastroh07@gmail.com>
2022-09-12 18:29:16 -0500

    comentarios agregados para la guia de mis compañeros

--- persona.php
+++ persona.php
@@ -1,4 +1,8 @@
 <?php
+    /**
+     * Declaramos variables iniciales para poder luego mantener los datos antes de enviar
+     * en caso de ue falte completar algun campo
+     */
     $dni = "";
     $nombre = "";
     $apellido = "";
@@ -6,10 +10,17 @@
     $telefono = "";
     $correo = "";
     $fecha = "";
-
+    /***
+     * Declaramos un arreglo de errores donde iremos agregando cada error segun el campo
+     * que no estee completado
+     */
     $errores = [];
 
+    // le decimos a la pagina si hay alguna peticion por el metodo POST que guarde
+    // los datos en las varaiables anteriores
+
     if ($_POST) {
+        // guardamos los datos en las variables
         $dni = $_POST["txtDni"];
         $nombre = $_POST["txtNombre"];
         $apellido = $_POST["txtApellido"];
@@ -17,6 +28,9 @@
         $telefono = $_POST["txtTelefono"];
         $correo = $_POST["txtCorreo"];
         $fecha = $_POST["txtFecha"];
+        
+        // Con trim le quitamos los espacios en blanco y le decimos ue si no hay nada agregue
+        // algo al arreglo de errores
 
         if (!trim($dni)) { array_push($errores,"El dni es necesario"); }
         if ( strlen(trim($dni))<=7) { array_push($errores,"El dni requiere 8 digitos"); }
@@ -27,6 +41,12 @@
         if (!trim($telefono)) { array_push($errores,"El telefono es necesario"); }
         if (!trim($correo)) { array_push($errores,"El correo es necesario"); }
         if (!trim($fecha)) { array_push($errores,"La fecho es necesario"); }
+
+        /**
+         *  Con la funcion count() validamos el tamaño del arreglo si no hay nada limpiamos los campos
+         *  en caso de trabajar con alguna base de datos o querer usar los datos ingresados antes de limpiar
+         *  podriamos realizar dichas funciones
+         */
 
         if (count($errores) == 0) {
             $dni = "";
@@ -45,7 +65,9 @@ include("./templates/header.php"); ?>
     <hr>
 </div>
 <div class="container">
-    <?php foreach($errores as $error): ?>
+    <?php
+    // hacemos uso del foreach para saber recorrer los errores en caos haya y si no simplemente no se mostrara
+     foreach($errores as $error): ?>
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
             <strong><?php echo $error; ?></strong>
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -53,7 +75,9 @@ include("./templates/header.php"); ?>
             </button>
         </div>
     <?php endforeach; ?>
-    <?php if(count($errores)==0 && $_POST): ?>
+    <?php
+    // en caso no haya errores y se haya realizado alguna petision POST notificamos Formulario envido con exito
+    if(count($errores)==0 && $_POST): ?>
         <div class="alert alert-success alert-dismissible fade show" role="alert">
             <strong><?php echo "Formulario enviado con exito"; ?></strong>
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -61,6 +85,7 @@ include("./templates/header.php"); ?>
             </button>
         </div>
     <?php endif; ?>
+    <!-- En el formulario sera validado en el mismo archivo por eso el action autoapuntamos el archivo  -->
     <form action="./persona.php" method="POST" class="border rounded p-2 m-auto" style="max-width:500px;">
         <div class="form-group">
             <label for="dni">DNI</label>
